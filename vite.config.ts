b7fd4ec156bbe257ef33b94b22e21f5e1456a9df
Connor Dirks <cdirks4@me.com>
2024-06-10 13:58:38 -0400

    feat(config): remix flat routes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { devErrorBoundary } from "@metronome-sh/dev-error-boundary";
+import { flatRoutes } from "remix-flat-routes";
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,9 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+      },
+      routes: async (defineRoutes) => {
+        return flatRoutes("routes", defineRoutes);
       },
       ignoredRouteFiles: ["**/*.css"],
       serverBuildFile: "index.js",
