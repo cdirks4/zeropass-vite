7ed33a9cfde6734bfaf7b82e5da1228414d949f6
Connor Dirks <cdirks4@me.com>
2024-06-10 13:59:31 -0400

    feat(config): add google fonts

--- app/root.tsx
+++ app/root.tsx
@@ -13,6 +13,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
