a5b8b4eb8c1432120342659b812aaa6b8f0a8380
Connor Dirks <80544198+cdirks4@users.noreply.github.com>
2024-07-24 16:20:00 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: { fontFamily: { sans: ["Urbanist", "sans-serif"] } },
